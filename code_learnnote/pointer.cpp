81392a623af58f3cd4d72125048c73c61dbb3630
jiumujin <3228582384@qq.com>
2025-12-02 18:15:16 +0800

    add note

--- code_learnnote/pointer.cpp
+++ code_learnnote/pointer.cpp
@@ -5,6 +5,10 @@ int main(){
     //指针：数据类型（与指向函数类型一致）*变量名
     int *p=&a;
     printf("Value of a: %d\n", a);
+    //利用指针存储/修改数据
+    *p=20;
+    printf("New value of a: %d\n", a);
+    //输出指针变量存储的地址
     printf("Address of a: %p\n", (void*)&a);
 
     return 0;
